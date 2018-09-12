b955f28b2e36e3081b46fdcf6750688654f14d34
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-12 23:19:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,10 +67,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    sql = "SELECT * FROM dogs"
-    DB[:conn].execute(sql).map do |row|
-      self.create(name: row[1], breed: row[2])
-    end
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_by_name(name)
